7be9f07c0c48dd1b9bc13a9a5d0b90a4b3c9688c
prizr0ck <helperfixelectronics@gmail.com>
2024-01-17 21:08:31 +0200

    fix parse_search

--- djangoApiSearch/settings.py
+++ djangoApiSearch/settings.py
@@ -24,7 +24,6 @@ SECRET_KEY = 'django-insecure-1pgqk&dp4o9oz1_3+e2(t@x69m4^u8ti)8clc_#tt8_+$-jpcy
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '103.45.247.96']
 INTERNAL_IPS = ['127.0.0.1', '103.45.247.96']
 CSRF_TRUSTED_ORIGINS = ['http://103.45.247.96']
--- scraping/parse_search.py
+++ scraping/parse_search.py
@@ -16,17 +16,27 @@ async def parse_eldorado(search):
     try:
         good_el = bs_el.find("div", {"class": "TileBlockstyled__StyledTileBlock-sc-ogrpyx-3 "
                                               "bwJliX OfferTilestyled__StyledTileBlock-sc-1lnuwp1-0 jUUHxV"})
-        link_el = good_el.find("a", {"class": "GoodsDescriptionstyled__StyledLinkWrapper-sc-1c1eyhs-0 gbyxYE"})
+    except Exception as e:
+        print(e)
+        good_el = None
+    try:
+        link_el = good_el.find("a", {"class": "GoodsDescriptionstyled__StyledLinkWrapper-sc-1c1eyhs-0 gbyxYE"})["href"]
+    except Exception as e:
+        print(e)
+        link_el = ""
+    try:
         name_el = good_el.find("span", {"class": "ui-library-body2Medium-fa40 "
-                                                 "GoodsDescriptionstyled__StyledTypography-sc-1c1eyhs-1 bDXGew"})
-        price_el = good_el.find("span", {"class": "ui-library-subtitle1Bold-399e"})
-
-        eldorado_list = ["Eldorado", eldorado_main + link_el["href"], name_el["title"], price_el.text + "₴"]
-        return eldorado_list
-    except Exception as error:
-        print(error)
-        eldorado_list = ["Eldorado", "-", "I'm afraid we don't have it", "-"]
-        return eldorado_list
+                                        "GoodsDescriptionstyled__StyledTypography-sc-1c1eyhs-1 bDXGew"})["title"]
+    except Exception as e:
+        print(e)
+        name_el = search
+    try:
+        price_el = good_el.find("span", {"class": "ui-library-subtitle1Bold-399e"}).text + "₴"
+    except Exception as e:
+        print(e)
+        price_el = "I'm afraid we don't have it"
+    eldorado_list = ["Eldorado", eldorado_main + link_el, name_el, price_el]
+    return eldorado_list
 
 
 async def parse_stilus(search):
@@ -34,13 +44,26 @@ async def parse_stilus(search):
     bs_stilus = BeautifulSoup(rq_stilus, 'html5lib')
     try:
         good_stilus = bs_stilus.find("a", {"class": "name-block"})
+    except Exception as e:
+        print(e)
+        good_stilus = None
+    try:
+        name_stilus = good_stilus["title"]
+    except Exception as e:
+        print(e)
+        name_stilus = search
+    try:
+        link_stilus = good_stilus["href"]
+    except Exception as e:
+        print(e)
+        link_stilus = ""
+    try:
         price_stilus = bs_stilus.find("div", {"class": "regular-price"}).text
-        stilus_list = ["Stilus", stilus_main + good_stilus["href"], good_stilus["title"], price_stilus]
-        return stilus_list
-    except Exception as error:
-        print(error)
-        stilus_list = ["Stilus", "-", "I'm afraid we don't have it", "-"]
-        return stilus_list
+    except Exception as e:
+        print(e)
+        price_stilus = "I'm afraid we don't have it"
+    stilus_list = ["Stilus", stilus_main + link_stilus, name_stilus, price_stilus]
+    return stilus_list
         
         
 async def parse_moyo(search):
@@ -48,16 +71,26 @@ async def parse_moyo(search):
     bs_moyo = BeautifulSoup(rq_moyo, 'html5lib')
     try:
         good_moyo = bs_moyo.find("a", {"class": "product-item_name gtm-link-product"})
-        price_moyo = bs_moyo.find("div", {"class": "product-item_price_current"}).text.strip()
-        moyo_list = ["Moyo",
-                     moyo_main + good_moyo["href"],
-                     good_moyo.text.strip(),
-                     price_moyo.replace(" ", "")]
-        return moyo_list
-    except Exception as error:
-        print(error)
-        moyo_list = ["Moyo", "-", "I'm afraid we don't have it", "-"]
-        return moyo_list
+    except Exception as e:
+        print(e)
+        good_moyo = None
+    try:
+        name_moyo = good_moyo.text.strip()
+    except Exception as e:
+        print(e)
+        name_moyo = search
+    try:
+        link_moyo = good_moyo["href"]
+    except Exception as e:
+        print(e)
+        link_moyo = ""
+    try:
+        price_moyo = bs_moyo.find("div", {"class": "product-item_price_current"}).text.strip().replace(" ", "")
+    except Exception as e:
+        print(e)
+        price_moyo = "I'm afraid we don't have it"
+    moyo_list = ["Moyo", moyo_main + link_moyo, name_moyo, price_moyo]
+    return moyo_list
         
         
 async def main(client_search):
