e3aee7a7686d6edde50e6d6410b63416018ec4b1
prizr0ck <helperfixelectronics@gmail.com>
2024-01-18 19:52:39 +0200

    installed swagger

--- djangoApiSearch/urls.py
+++ djangoApiSearch/urls.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from scraping.views import SearchViewSet, GoodsViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Scraping",
+        default_version='v1',
+        description="API for scraping",
+        terms_of_service="https://www.ourapp.com/policies/terms/",
+        contact=openapi.Contact(email="contact@swaggerBlog.local"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 Router = routers.DefaultRouter()
 Router.register('search', SearchViewSet, basename='search')
@@ -9,5 +24,7 @@ Router.register('goods', GoodsViewSet, basename='goods')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(Router.urls))
+    path('api/', include(Router.urls)),
+    path('doc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
